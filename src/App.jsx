3b13f75f657a4a23aac684453bb892a1e18e10d7
abidt20ahmed <abidahmedmahin21@gmail.com>
2023-06-08 02:37:34 +0600

    Router setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Flowbite>
+      <NavBar></NavBar>
+      ...
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <h1 className=' dark:text-red-500'>sadkfjdsfkdsajfdsak</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,7 +26,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+
+      <DarkThemeToggle />
+      ...
+    </Flowbite>
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+import router from './Routes/Routes';
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+import AuthProvider from './context/AuthProvider';
+// import router from './Routes/Routes';
+// import AuthProvider from './context/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Flowbite>
+    <div className=' bg-indigo-100 dark:bg-slate-900'>
+      <React.StrictMode>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <DarkThemeToggle />
+          <ToastContainer />
+        </AuthProvider>
+      </React.StrictMode>
+    </div>,
+  </Flowbite>
 )
